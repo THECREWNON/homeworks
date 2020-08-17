88d8553f18ffd425d9f25b4f1cdb489db43ca5e4
THECREWNON <littledembone@gmail.com>
2020-08-17 16:56:58 +0300

    homework two finish commit

--- homework2/script.js
+++ homework2/script.js
@@ -1,13 +1,33 @@
-let num = 123
-console.log (num)
-
+//первое задание
+let w = 123;
+console.log (w)
+//второе задание
 let a = 10;
 console.log(a)
-let a = 20;
-console.log(a)
-
-let a = 1;
-let b = 2;
-let c = 3;
-let result = a+b+c;
-
+//третье задание
+let b = 1;
+let c = 2;
+let d = 3;
+console.log(b+c+d)
+//четвортое задание
+let result = 4 + 7 + 5;
+console.log(result)
+//пятое задание
+let e = 10;
+let f = 5;
+let z = e - f;
+let g = 7;
+let x = z + g;
+console.log(x)
+//шестое задание 
+let num = 5;
+let fakeNum = '1';
+console.log(num + fakeNum)
+/*получилось потому что цыфра 1 написана в строчке ('1') 
+а цыфры которые написаны в строке не плюсуютса */
+//седьмое задание
+console.log(typeof 42);
+console.log(typeof 'blubber');
+console.log(typeof true);
+console.log(typeof undeclaredVariable);
+console.log(typeof {})
